2343d9454aff22455d6dc4552e8d9006ee6c1d50
animek12 <111511065+animek12@users.noreply.github.com>
2022-10-28 12:51:20 +0800

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -9,7 +9,7 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 let media = global.media
-  let kri = `FIRST THANKS = KRIZYNOFC
+  let kri = `FIRST THANKS = FINOFC
 NOTE : *Telah Mengembangkan Bot*
   
 TWO THANKS = WHATSAPP OFFCIAL
@@ -25,6 +25,8 @@ let ofc = ` ${hki} THANKS CREATOR ${hka}
     📮 https://github.com/Hyzerr
 ⤿ Krizyn Ofc
     📮 https://github.com/krizynofc
+⤿ Fin Ofc
+    📮 https://github.com/Raya2399
     
 THANKS PARA DONASI YANG TERHORMAT`
 
